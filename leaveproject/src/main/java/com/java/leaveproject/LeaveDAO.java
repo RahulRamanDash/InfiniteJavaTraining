b3552e3dc8e88d68147a66bf88a75bf906145247
Rahul Raman Dash <84366398+RahulRamanDash@users.noreply.github.com>
2023-09-03 20:22:51 +0530

    Add files via upload

---
+++ leaveproject/src/main/java/com/java/leaveproject/LeaveDAO.java
@@ -0,0 +1,14 @@
+package com.java.leaveproject;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.List;
+
+public interface LeaveDAO {
+	List<LeaveDetails> showLeaveDao();
+	String addLeaveDao(LeaveDetails leavedata) throws ParseException;
+	int incrimentLeaveID();
+	String writeLeaveFileDao() throws IOException;
+	String ReadLeaveFileDao() throws IOException, ClassNotFoundException;
+	
+}
