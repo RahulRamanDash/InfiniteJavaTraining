79d524d56d2b4531ba09d5bcd4e82e40a486ec9e
Rahul Raman Dash <84366398+RahulRamanDash@users.noreply.github.com>
2023-09-27 11:18:36 +0530

    Add files via upload

---
+++ Project/JDBC/InventoryProject/src/test/java/com/java/inventoryproject/AppTest.java
@@ -0,0 +1,14 @@
+package com.java.inventoryproject;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+public class AppTest {
+
+  @Test
+  public void testMain() throws ClassNotFoundException, SQLException {
+    App.main(null);
+  }
+
+}
